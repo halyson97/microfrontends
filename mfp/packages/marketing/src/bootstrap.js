a4c4d662de2c59dc9263cfe0265aaf52e6ae6ca4
Halyson Rezende <halyson97@gmail.com>
2024-02-15 01:48:03 -0300

    feat: render marketing components

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 // Mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(
-        <h1>Hi there!</h1>,
-        el
-    );
+    ReactDOM.render(<App />, el);
 };
 
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-rooot');
-    
+
     if (devRoot) {
         mount(devRoot);
     }
