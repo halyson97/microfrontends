7dd5812581310a20a5a0e65ee334e103537c5f65
Halyson Rezende <halyson97@gmail.com>
2024-02-18 23:42:12 -0300

    feat: integrate auth app

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
@@ -15,7 +17,12 @@ export default () => {
             <BrowserRouter>
                 <div>
                     <Header />
-                    <MarketingApp />
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path="/auth" component={MarketingLazy} />
+                            <Route path="/" component={AuthLazy} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </BrowserRouter>
         </StylesProvider>
